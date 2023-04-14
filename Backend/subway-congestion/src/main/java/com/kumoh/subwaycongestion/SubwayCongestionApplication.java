25eb6135629e6a5a0d3bee321b96ef853012580f
skwogus98 <skwogus98@gmail.com>
2023-04-14 13:10:23 +0900

    backend init

---
+++ Backend/subway-congestion/src/main/java/com/kumoh/subwaycongestion/SubwayCongestionApplication.java
@@ -0,0 +1,13 @@
+package com.kumoh.subwaycongestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayCongestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubwayCongestionApplication.class, args);
+	}
+
+}
---
+++ Backend/subway-congestion/src/test/java/com/kumoh/subwaycongestion/SubwayCongestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumoh.subwaycongestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayCongestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
